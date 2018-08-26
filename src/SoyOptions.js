35bdf9171390969fc4be9ac281008191556de331
vikr01 <vikr01@users.noreply.github.com>
2018-08-26 03:08:42 -0700

    made SoyOptions into a class

--- src/SoyOptions.js
+++ src/SoyOptions.js
@@ -6,190 +6,190 @@ import path from 'path';
  * Describes the possible options set to a SoyCompiler.
  * @constructor
  */
-function SoyOptions() {
-  /**
-   * A temporary directory where compiled .soy.js files will be stored after compilation.
-   * @type {string}
-   */
-  this.tmpDir = '/tmp/soynode';
+export default class SoyOptions {
+  constructor() {
+    /**
+     * A temporary directory where compiled .soy.js files will be stored after compilation.
+     * @type {string}
+     */
+    this.tmpDir = '/tmp/soynode';
+
+    /**
+     * Directory where the compiler will spawn compilation process.
+     * When compiling from files defaults to process.cwd(), if compiling from a directory inputDir is used instead.
+     * @type {string}
+     */
+    this.inputDir = process.cwd();
+
+    /**
+     * An output directory, which compiled soy.js files is stored.
+     * @type {?string}
+     */
+    this.outputDir = null;
+
+    /**
+     * A directory of precompiled soy.js files. Soynode will check these first and
+     * use them if available.
+     *
+     * You can set this to the same value as outputDir to re-use
+     * results from previous runs.
+     *
+     * @type {?string}
+     */
+    this.precompiledDir = null;
+
+    /**
+     * Whether the compiled soy files should be placed into a unique directory(timestamped).
+     * @type {boolean}
+     */
+    this.uniqueDir = true;
+
+    /**
+     * Whether to watch any files that are loaded and to refetch them when they change.
+     * @type {boolean}
+     */
+    this.allowDynamicRecompile = false;
+
+    /**
+     * Whether or not to load the compiled templates in the VM context.
+     * @type {boolean}
+     */
+    this.loadCompiledTemplates = true;
+
+    /**
+     * Whether to delete temporary files created during the compilation process.
+     * @type {boolean}
+     */
+    this.eraseTemporaryFiles = false;
+
+    /**
+     * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
+     * @type {boolean}
+     */
+    this.useClosureStyle = false;
+
+    /**
+     * Whether or not to generate JSDoc on each template function, with type info for the Closure Compiler.
+     * @type {boolean}
+     */
+    this.shouldGenerateJsdoc = false;
+
+    /**
+     * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
+     * If you set this flag, each generated JavaScript file contains:
+     * - one goog.provide statement for the corresponding Soy file's namespace
+     * - goog.require statements for the namespaces of the called template
+     * @type {boolean}
+     */
+    this.shouldProvideRequireSoyNamespaces = false;
+
+    /**
+     * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
+     * If you set this flag, each generated JS file contains:
+     * - goog.provide statements for the full names of each of its template JS functions
+     * - goog.require statements for the full names of each of the called templates.
+     * @type {boolean}
+     */
+    this.shouldProvideRequireJsFunctions = false;
+
+    /**
+     * The scheme to use for handling 'css' commands. Specifying
+     * 'literal' will cause command text to be inserted as literal
+     * text. Specifying 'reference' will cause command text to be
+     * evaluated as a data or global reference. Specifying 'goog'
+     * will cause generation of calls goog.getCssName. This option
+     * has no effect if the Soy code does not contain 'css'
+     * commands.
+     * @type {?string}
+     */
+    this.cssHandlingScheme = undefined;
+
+    /**
+     * Additional classpath to pass to the soy template compiler. This makes adding plugins possible.
+     * @type {Array<string>}
+     */
+    this.classpath = [];
+
+    /**
+     * Plugin module Java classnames to pass to the soy template compiler.
+     * @type {Array<string>}
+     */
+    this.pluginModules = [];
+
+    /**
+     * Additional JS files to be evaluated in the VM context for the soy templates.
+     * Useful for soy function support libs
+     * @type {Array<string>}
+     */
+    this.contextJsPaths = [];
+
+    /**
+     * Whether the compiled soy.js files should be joined into a single file
+     * @type {boolean}
+     */
+    this.concatOutput = false;
+
+    /**
+     * File name used for concatenated files, only relevant when concatOutput is true.
+     * @type {string}
+     */
+    this.concatFileName = 'compiled';
+
+    /**
+     * List of locales to translate the templates to.
+     * @type {Array<string>}
+     */
+    this.locales = [];
+
+    /**
+     * Path to the translation file to use, which can contain any of the placeholders
+     * allowed on the --messageFilePathFormat option of SoyToJsSrcCompiler.jar.
+     * @type {?string}
+     */
+    this.messageFilePathFormat = null;
+
+    /**
+     * When this option is set to false, each generated JS file
+     * will not attempt to declare the top-level name in its
+     * namespace, instead assuming the top-level name is already
+     * declared in the global scope. E.g. for namespace aaa.bbb,
+     * the code will not attempt to declare aaa, but will still
+     * define aaa.bbb if it's not already defined.
+     * @type {boolean}
+     */
+    this.shouldDeclareTopLevelNamespaces = true;
+
+    /**
+     * Points to a directory with proto files
+     * @type {string}
+     */
+    this.protoFileDescriptors = '';
+  }
 
   /**
-   * Directory where the compiler will spawn compilation process.
-   * When compiling from files defaults to process.cwd(), if compiling from a directory inputDir is used instead.
-   * @type {string}
+   * Sets options which affect how soynode operates.
    */
-  this.inputDir = process.cwd();
+  merge(opts) {
+    for (const key in opts) {
+      const isFunction = typeof this[key] === 'function';
+      if (isFunction && this[key] === opts[key]) {
+        continue;
+      }
 
-  /**
-   * An output directory, which compiled soy.js files is stored.
-   * @type {?string}
-   */
-  this.outputDir = null;
+      if (!(key in this) || typeof this[key] === 'function') {
+        throw new Error(`soynode: Invalid option key [${key}]`);
+      }
 
-  /**
-   * A directory of precompiled soy.js files. Soynode will check these first and
-   * use them if available.
-   *
-   * You can set this to the same value as outputDir to re-use
-   * results from previous runs.
-   *
-   * @type {?string}
-   */
-  this.precompiledDir = null;
-
-  /**
-   * Whether the compiled soy files should be placed into a unique directory(timestamped).
-   * @type {boolean}
-   */
-  this.uniqueDir = true;
-
-  /**
-   * Whether to watch any files that are loaded and to refetch them when they change.
-   * @type {boolean}
-   */
-  this.allowDynamicRecompile = false;
-
-  /**
-   * Whether or not to load the compiled templates in the VM context.
-   * @type {boolean}
-   */
-  this.loadCompiledTemplates = true;
-
-  /**
-   * Whether to delete temporary files created during the compilation process.
-   * @type {boolean}
-   */
-  this.eraseTemporaryFiles = false;
-
-  /**
-   * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
-   * @type {boolean}
-   */
-  this.useClosureStyle = false;
-
-  /**
-   * Whether or not to generate JSDoc on each template function, with type info for the Closure Compiler.
-   * @type {boolean}
-   */
-  this.shouldGenerateJsdoc = false;
-
-  /**
-   * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
-   * If you set this flag, each generated JavaScript file contains:
-   * - one goog.provide statement for the corresponding Soy file's namespace
-   * - goog.require statements for the namespaces of the called template
-   * @type {boolean}
-   */
-  this.shouldProvideRequireSoyNamespaces = false;
-
-  /**
-   * Whether or not to use goog.provide and goog.require for JS functions and Soy namespaces.
-   * If you set this flag, each generated JS file contains:
-   * - goog.provide statements for the full names of each of its template JS functions
-   * - goog.require statements for the full names of each of the called templates.
-   * @type {boolean}
-   */
-  this.shouldProvideRequireJsFunctions = false;
-
-  /**
-   * The scheme to use for handling 'css' commands. Specifying
-   * 'literal' will cause command text to be inserted as literal
-   * text. Specifying 'reference' will cause command text to be
-   * evaluated as a data or global reference. Specifying 'goog'
-   * will cause generation of calls goog.getCssName. This option
-   * has no effect if the Soy code does not contain 'css'
-   * commands.
-   * @type {?string}
-   */
-  this.cssHandlingScheme = undefined;
-
-  /**
-   * Additional classpath to pass to the soy template compiler. This makes adding plugins possible.
-   * @type {Array<string>}
-   */
-  this.classpath = [];
-
-  /**
-   * Plugin module Java classnames to pass to the soy template compiler.
-   * @type {Array<string>}
-   */
-  this.pluginModules = [];
-
-  /**
-   * Additional JS files to be evaluated in the VM context for the soy templates.
-   * Useful for soy function support libs
-   * @type {Array<string>}
-   */
-  this.contextJsPaths = [];
-
-  /**
-   * Whether the compiled soy.js files should be joined into a single file
-   * @type {boolean}
-   */
-  this.concatOutput = false;
-
-  /**
-   * File name used for concatenated files, only relevant when concatOutput is true.
-   * @type {string}
-   */
-  this.concatFileName = 'compiled';
-
-  /**
-   * List of locales to translate the templates to.
-   * @type {Array<string>}
-   */
-  this.locales = [];
-
-  /**
-   * Path to the translation file to use, which can contain any of the placeholders
-   * allowed on the --messageFilePathFormat option of SoyToJsSrcCompiler.jar.
-   * @type {?string}
-   */
-  this.messageFilePathFormat = null;
-
-  /**
-   * When this option is set to false, each generated JS file
-   * will not attempt to declare the top-level name in its
-   * namespace, instead assuming the top-level name is already
-   * declared in the global scope. E.g. for namespace aaa.bbb,
-   * the code will not attempt to declare aaa, but will still
-   * define aaa.bbb if it's not already defined.
-   * @type {boolean}
-   */
-  this.shouldDeclareTopLevelNamespaces = true;
-
-  /**
-   * Points to a directory with proto files
-   * @type {string}
-   */
-  this.protoFileDescriptors = '';
-}
-
-/**
- * Sets options which affect how soynode operates.
- */
-SoyOptions.prototype.merge = function(opts) {
-  for (const key in opts) {
-    const isFunction = typeof this[key] === 'function';
-    if (isFunction && this[key] === opts[key]) {
-      continue;
-    }
-
-    if (!(key in this) || typeof this[key] === 'function') {
-      throw new Error(`soynode: Invalid option key [${key}]`);
-    }
-
-    // When setting the tmpDir make sure to resolve the absolute path so as to avoid accidents
-    // caused by changes to the working directory.
-    if (key === 'tmpDir') {
-      this.tmpDir = path.resolve(opts.tmpDir);
-    } else if (key === 'outputDir') {
-      this.outputDir =
-        opts.outputDir == null ? null : path.resolve(opts.outputDir);
-    } else {
-      this[key] = opts[key];
+      // When setting the tmpDir make sure to resolve the absolute path so as to avoid accidents
+      // caused by changes to the working directory.
+      if (key === 'tmpDir') {
+        this.tmpDir = path.resolve(opts.tmpDir);
+      } else if (key === 'outputDir') {
+        this.outputDir =
+          opts.outputDir == null ? null : path.resolve(opts.outputDir);
+      } else {
+        this[key] = opts[key];
+      }
     }
   }
-};
-
-export default SoyOptions;
+}
